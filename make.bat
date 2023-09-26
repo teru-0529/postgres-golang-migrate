4c2cfbf565f3b1984d26378c1eea8482e476b159
teru-0529 <andrea.pirlo.0529@gmail.com>
2023-09-26 19:02:19 +0900

    マイグレーションコマンド登録

--- make.bat
+++ make.bat
@@ -14,10 +14,13 @@ if "%1"=="" (
   echo down           :コンテナ停止
   echo migrate-create :マイグレーションファイルの生成[名称用に第2引数が必要]
   echo migrate-up     :最新状態にマイグレーション実施
-  echo migrate-down   :マイグレーションファイルの生成
-  echo migrate-force  :指定したバージョンにマイグレーションを実行[バージョン指定用に第2引数が必要]
+  echo migrate-down   :初期状態にマイグレーションロールバック
+  echo migrate-goto   :指定したバージョンにマイグレーションを実行[バージョン指定用に第2引数が必要]
   exit /b
 )
+
+set DATABASE_URL="postgres://%POSTGRES_USER%:%POSTGRES_PASSWORD%@db:%POSTGRES_PORT%/%POSTGRES_DB%?sslmode=disable"
+set DIR_PATH="./src/database/migrations"
 
 rem コンテナ起動
 if %1==up (
@@ -35,14 +38,24 @@ if %1==migrate-create (
     echo マイグレーションファイル名として第2引数が必要です。
     exit /b
   )
-  echo docker-compose exec app migrate create -ext sql -dir ./src/database/migrations -seq %2
+  docker-compose exec app migrate create -ext sql -dir %DIR_PATH% -seq %2
 )
 
+rem マイグレーション実行
+if %1==migrate-up (
+  docker-compose exec app migrate -database %DATABASE_URL% -path %DIR_PATH% up
+)
 
+rem マイグレーションロールバック
+if %1==migrate-down (
+  docker-compose exec app migrate -database %DATABASE_URL% -path %DIR_PATH% down
+)
 
-set DATABASE_URL="postgres://%POSTGRES_USER%:%POSTGRES_PASSWORD%@db:%POSTGRES_PORT%/%POSTGRES_DB%?sslmode=disable"
-@REM echo %DATABASE_URL%
-
-@REM docker-compose exec app migrate -database "postgres://${ユーザー名}:${パスワード}@${ホスト}:${ポート}/${データベースの名前}?sslmode=disable"
-
-
+rem マイグレーションを実行
+if %1==migrate-goto (
+  if "%2"=="" (
+    echo バージョンの指定が必要です。
+    exit /b
+  )
+  docker-compose exec app migrate -database %DATABASE_URL% -path %DIR_PATH% goto %2
+)
